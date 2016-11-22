c5d0369b76b9bde42ca3c3f7db0a7c36c8d18aa6
Mario van Antwerpen <mario@fiorano.net>
2016-11-22 16:40:34 +0100

    Added parameters and verbose messages

--- IIS/NewFtpSite.ps1
+++ IIS/NewFtpSite.ps1
@@ -1,15 +1,27 @@
-$FtpSiteName = 'FTPSite'
-$FtpRootPath = 'c:\inetpub\ftproot'
-$FtpUserName = 'user1'
-$ftpUserPath = 'c:\inetpub\wwwroot'
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$FtpSiteName='FTPSite',
+	
+    [Parameter(Mandatory=$True)]
+    [string]$FtpRootPath,
+
+    [Parameter(Mandatory=$True)]
+    [string]$FtpUserName,
+
+    [Parameter(Mandatory=$True)]
+    [string]$ftpUserPath
+)
 
 # Create FTP Folder
 if ((Test-Path $FtpRootPath) -eq $false) {
+    Write-Verbose -Message "Creating folder $FtpRootPath"
     New-Item -Path $FtpRootPath -ItemType Directory
 }
 
 # Create FtpSite
 if ((Get-Website -Name $FtpSiteName) -eq $null) {
+    Write-Verbose -Message "Creating new FTP Site."
     New-WebFtpSite -Name $FtpSiteName -Port 21 -IPAddress '*' -PhysicalPath $FtpRootPath
 } else {
     Write-Error -Message "$FtpSiteName already exists!"
@@ -17,6 +29,7 @@ if ((Get-Website -Name $FtpSiteName) -eq $null) {
 
 # Make localuser virtual directory
 if ((Get-WebVirtualDirectory -name 'LocalUser') -eq $null) {
+    Write-Verbose -Message "Creating LocalUser virtual directory for user isolation."
     New-WebVirtualDirectory -Site $FtpSiteName -Name 'LocalUser' -PhysicalPath $FtpRootPath
 } else {
     Write-Error -Message "Virtual directory 'LocalUser' at $FtpSiteName already exists!"
@@ -24,6 +37,7 @@ if ((Get-WebVirtualDirectory -name 'LocalUser') -eq $null) {
 
 # Make FtpUser Virtual Directory
 if ((Get-WebVirtualDirectory -name "LocalUser/$FtpUserName") -eq $null) {
+    Write-Verbose -Message "Creating LocalUser/$FtpUserName virtual directory for user isolation."
     New-WebVirtualDirectory -Site "$FtpSiteName\localuser" -Name $FtpUserName -PhysicalPath $ftpUserPath
     #Start-Sleep -Seconds 5
 } else {
@@ -31,6 +45,6 @@ if ((Get-WebVirtualDirectory -name "LocalUser/$FtpUserName") -eq $null) {
 }
 
 # Configure FTP Authorization rule for FtpUser Virtual Directory 
-Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST' -location "FTPSite/LocalUser/$FtpUserName" -filter "system.ftpServer/security/authorization" -name "." -AtElement @{users='*';roles='';permissions='1'}
-Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -location "FTPSite/LocalUser/$FtpUserName" -filter "system.ftpServer/security/authorization" -name "." -value @{accessType='Allow';users='*';permissions='Read,Write'}
-
+Write-Verbose -Message "Configuring authorization rule for LocalUser/$FtpUserName virtual directory."
+Remove-WebConfigurationProperty  -pspath 'MACHINE/WEBROOT/APPHOST' -location "$FtpSiteName/LocalUser/$FtpUserName" -filter "system.ftpServer/security/authorization" -name "." -AtElement @{users='*';roles='';permissions='1'}
+Add-WebConfigurationProperty -pspath 'MACHINE/WEBROOT/APPHOST' -location "$FtpSiteName/LocalUser/$FtpUserName" -filter "system.ftpServer/security/authorization" -name "." -value @{accessType='Allow';users='*';permissions='Read,Write'}
