c1f209207144e693ce0e0066389cdb462ee267c3
Mario van Antwerpen <mario@fiorano.net>
2016-11-25 10:28:49 +0100

    Added paramters and verbose messages

---
+++ IIS/InstallWebServerRole.ps1
@@ -0,0 +1,69 @@
+<#
+.Synopsis
+   This script install the webserver role on a windows server.
+.DESCRIPTION
+   This script install the webserver role on a windows server with its defaukt settings. You can also install additional webserver role features. 
+   After installing it enables the firewall for HTTP and disables the firewall rule foor HTTPS. 
+.EXAMPLE
+   This example installs just the default webserver role.
+
+   InstallWebserverRole.ps1
+.EXAMPLE
+   This example installs the webserver role including some additional features.
+
+   InstallWebserverRole.ps1 -IncludeAdditionalWebFeatures
+.EXAMPLE
+   This example installs the webserver role including some additional features. And gives verbose output.
+
+   InstallWebserverRole.ps1 -IncludeAdditionalWebFeatures -Verbose
+#>
+[CmdletBinding()]
+Param
+(
+    # If this parameter is used The additional webfeatures like will be installed. Default state is False.
+    [Parameter(
+        Mandatory=$false,
+        HelpMessage="Use this parameter to also install additional webfeatures")
+    ]
+    [switch]$IncludeAdditionalWebFeatures=$false
+)
+
+# Install webserver role
+Write-Verbose -Message 'Installing webserver role'
+$result = Install-WindowsFeature -Name 'web-server'
+if ($result.Success) {
+    Write-Verbose -Message "Installation of the webserver role was succesful."
+    if ($result.ExitCode -eq 'SuccessRestartRequired') {
+        Write-Verbose -Message "To complete the installation you need to restart the server." 
+    }
+    if ($result.ExitCode -ne 'NoChangeNeeded') { 
+        Write-Verbose -Message "The following modules are installed:"
+        Write-Verbose -Message "$($result.FeatureResult | Select DisplayName, Success)"
+    }
+}
+$result = $null
+
+if ($IncludeAdditionalWebFeatures) {
+    # Install Additional Web Server Features
+    $AdditionalWebFeatures = "Web-Static-Content","Web-Http-Errors","Web-Http-Redirect","Web-Stat-Compression","Web-ISAPI-Ext","Web-ISAPI-Filter","Web-Http-Logging","Web-Request-Monitor","Web-Basic-Auth","Web-Filtering","Web-IP-Security","Web-Performance","Web-Asp","Web-Asp-Net","Web-Asp-Net45","Web-Mgmt-Console","Web-Ftp-Service"
+    Write-Verbose -Message "Installing additional webserver role features"
+    $result = Install-WindowsFeature -name $AdditionalWebFeatures
+    if ($result.Success) {
+        Write-Verbose -Message "Installation of the additional webserver role features was succesful."
+        if ($result.ExitCode -eq 'SuccessRestartRequired') {
+            Write-Verbose -Message "To complete the installation you need to restart the server." 
+        }
+        if ($result.ExitCode -ne 'NoChangeNeeded') { 
+            Write-Verbose -Message "The following modules are installed:"
+            Write-Verbose -Message "$($result.FeatureResult | Select DisplayName, Success)"
+        }
+    }
+} else {
+    Write-Verbose -Message "Skipping installing additional Webserver role features"
+}
+
+# Enable Built-In Firewall Rule for HTTP-IN for all profiles
+Write-Verbose -Message 'Enabling HTTP-IN firewall role for all profiles'
+Set-NetFirewallRule -Name 'IIS-WebServerRole-HTTP-In-TCP' -Profile 'Domain','Public','Private' -Enabled 'True'
+Write-Verbose -Message 'Disabling HTTPS-IN firewall role for all profiles'
+Set-NetFirewallRule -Name 'IIS-WebServerRole-HTTPS-In-TCP' -Profile 'Domain','Public','Private' -Enabled 'False'
