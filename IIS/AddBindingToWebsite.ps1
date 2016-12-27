9ca4ddc2389cc188e29a38355db6b3e4f4d1f640
Mario van Antwerpen <mario@fiorano.net>
2016-12-27 14:36:52 +0100

    Added help

--- IIS/AddBindingToWebsite.ps1
+++ IIS/AddBindingToWebsite.ps1
@@ -1,55 +1,37 @@
 ﻿#requires -Version 3 -Modules WebAdministration -RunAsAdministrator
-
 <#
-.Synopsis
+.SYNOPSIS
    With this script you can add one or more hostheaders to a website.
-
 .DESCRIPTION
    With this script you can add one or more hostheaders to a website. 
    All hostheaders will be bound the same IPAddress and Port.
    It is not possible to do an SSL binding.
-
-.PARAMETER
-   Name [string]
-
+.PARAMETER Name <string>
    This is name of the website you want to add one or more extra bindings to. For example Default Web Site.
-
-.PARAMETER
-   HostHeader [string[]]
-
+.PARAMETER HostHeader <string[]>
    This is one or more domain names you want to add to a website. The domain name can be www.test.com or test.com or blog.test.com.
    It is possible to add multiple domain names at the same time as long as these domain names will be bound to the same website, ipaddress and port.
-
-.PARAMETER
-   IPAddresss [string]
-
+.PARAMETER IPAddresss <string>
    This is the ipaddress you want to bind a domain name too. This parameter is optional. If you don't supply an ipaddress the default value '*' will
    be used. So the domain name will be bound to any ipaddress on the server.
-
-.PARAMETER
-   Port [int]
-
+.PARAMETER Port <int>
    This is an optional parameter with a default value of '80'. If you want to use another port you can supply it here.
-
+.NOTES
+   Version:         0.1.0
+   Author:          Mario van Antwerpen
+   Creation Date:   23-12-20016
+   Purpose/Change:  Initial script development
 .EXAMPLE
+   .\AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com","test.com"
    Add 2 new hostheaders to the "Default Web Site" for any IP Address on default port 80.
-
-   PS> AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com","test.com"
-
 .EXAMPLE
+   .\AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com" -IPAddress "127.0.0.1"
    Add a new hostheader to the "Default Web Site" for a specifc IP Address on default port 80.
-
-   PS> AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com" -IPAddress "127.0.0.1"
-
 .EXAMPLE
+   .\AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com","test.nl" -port 8080
    Add a new hostheader to the "Default Web Site" for any IP Address on port 8080.
 
-   PS> AddBindingToWebsite.ps1 -Name "Default Web Site" -HostHeader "www.test.com","test.nl" -port 8080
-
-.EXAMPLE
-   Another example of how to use this cmdlet
 #>
-
 [CmdletBinding(SupportsShouldProcess=$true,
                ConfirmImpact='High')]
 [Alias()]
